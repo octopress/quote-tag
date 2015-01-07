29de1a7448b65ea02e9a84dd0f2710ec2fab619c
Brandon Mathis <brandon@imathis.com>
2015-01-07 00:14:52 -0600

    version bump

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+### 1.0.1 (2015-01-07)
+
+- Fixed name typo in Octopress Docs
+
+### 1.0.0 (2015-01-06)
+
+- Initial release
--- lib/octopress-quote-tag/version.rb
+++ lib/octopress-quote-tag/version.rb
@@ -1,7 +1,7 @@
 module Octopress
   module Tags
     module QuoteTag
-      VERSION = "1.0.0"
+      VERSION = "1.0.1"
     end
   end
 end
